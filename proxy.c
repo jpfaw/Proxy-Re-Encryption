36aa27fb524e25e6dd05e149ee0b719d92a3bd54
jpfaw <isidoyuuta@gmail.com>
2018-07-11 14:48:23 +0900

    暗号化の実装

--- proxy.c
+++ proxy.c
@@ -9,7 +9,7 @@
 #include <tepla/ec.h>
 #include <openssl/sha.h>
 
-#define DEBUG 0 // 0: false 1: true
+#define DEBUG 1 // 0: false 1: true
 #define MESSAGE_SIZE 1024
 #define CODE_SIZE MESSAGE_SIZE/sizeof(long)
 
@@ -23,26 +23,27 @@ typedef enum{
 
 void print_red_color(const char *text);
 void print_green_color(const char *text);
+void create_mpz_t_random(mpz_t op, const mpz_t n);
 void data_check(const int data, const int assumption);
-/* -----------------------------------------------
- * unsigned char(SHA256でハッシュ化した値)を出力する関数
- * $0 出力するu_char
- * $1 データ名（出力の最初にprintされる）
- * $2 データサイズ
- -----------------------------------------------*/
-void print_unsigned_char(const unsigned char *uc, const char *dataName, const size_t size){
-    printf("\x1b[32m%s = \x1b[39m", dataName);
-    for (size_t i=0; i<size; i++){
-        printf("%02x", uc[i] );
-    }
-    printf("\n");
-}
+void print_unsigned_char(const unsigned char *uc, const char *dataName, const size_t size);
 
 int main(void) {
-    /* --- セットアップ --- */
+/* --- セットアップ --- */
     int i;
 //    char msg[MESSAGE_SIZE]="Hello World!";
     char msg[MESSAGE_SIZE]="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+    int msg_len = strlen(msg);
+    
+    /* --- 上限値の設定 --- */
+    mpz_t limit;
+    mpz_init(limit);
+    mpz_set_ui(limit, 2);
+    mpz_pow_ui(limit, limit, 254);
+    print_green_color("limit = ");
+    gmp_printf ("%s%Zd\n", "", limit);
+
+/* --- 暗号化 --- */
+    
     /* --- ペアリングを生成 --- */
     EC_PAIRING p;
     pairing_init(p, "ECBN254a");
@@ -60,13 +61,22 @@ int main(void) {
     point_random(Q);
     print_green_color("Q =  ");
     point_print(Q);
+    
+    data_check(point_is_on_curve(P) + point_is_on_curve(Q), 2);
+    
+    /* --- aとrを生成 --- */
+    mpz_t a, r;
+    mpz_init(a);
+    mpz_init(r);
+    create_mpz_t_random(a, limit);
+    create_mpz_t_random(r, limit);
 
-    /* -- g := e(P, Q)を生成 --- */
+    /* -- g = e(P, Q)^r を生成 --- */
     Element g;
     element_init(g, p->g3);
     pairing_map(g, P, Q, p);
+    element_pow(g, g, r);       // r乗する
     element_print(g);
-    data_check(point_is_on_curve(P) + point_is_on_curve(Q), 2);
 
     int element_size = element_get_str_length(g);
     char *g_element_str;
@@ -101,33 +111,88 @@ int main(void) {
     unsigned long enc_key[CODE_SIZE];
     memset(enc_key,0,sizeof(enc_key));
     //encode(msg)
-    memcpy(enc_msg,msg,strlen(msg));
-    for(i=0;i<strlen(msg)/sizeof(long)+1;i++){
-        printf("enc[%d]:%ld\n",i,enc_msg[i]);
-    }
+    memcpy(enc_msg,msg,msg_len);
+    if(DEBUG) for(i=0;i<msg_len/sizeof(long)+1;i++) printf("enc_msg[%d]:%ld\n",i,enc_msg[i]);
     //encode(g_key)
-    memcpy(enc_key,g_key,strlen(msg));
-    for(i=0;i<strlen(msg)/sizeof(long)+1;i++){
-        printf("enc[%d]:%ld\n",i,enc_key[i]);
+    memcpy(enc_key,g_key,msg_len);
+    if(DEBUG) for(i=0;i<msg_len/sizeof(long)+1;i++) printf("enc_key[%d]:%ld\n",i,enc_key[i]);
+
+    /* --- 文字列と鍵を掛け算 --- */
+    mpz_t u[msg_len];
+    for(i=0;i<msg_len/sizeof(long);i++) mpz_init(u[i]);
+    for(i=0;i<msg_len/sizeof(long);i++) {
+        mpz_t a, b;
+        mpz_init(a);
+        mpz_init(b);
+        mpz_set_ui(a, enc_msg[i]); // unsigned long -> mpz_t
+        mpz_set_ui(b, enc_key[i]);
+        mpz_mul(u[i], a, b); // mpz_t * mpz_t
+        if(DEBUG) gmp_printf ("u[%d]: %Zd\n", i, u[i]);
     }
     
+    /* --- r(aQ) を計算 --- */
+    EC_POINT aQ;
+    EC_POINT raQ;
+    point_init(aQ, p->g2);
+    point_init(raQ, p->g2);
+    point_mul(aQ, a, Q);
+    point_mul(raQ, r, aQ);
+    print_green_color("raQ = ");
+    point_print(raQ);
     
-//    unsigned char m[]  = "Hello_World!";
-//    Element z;
-//    element_init(z, p->g3);
-//    element_from_oct(z, m, sizeof(m));
-////    print_green_color("z = ");
-//    element_print(z);
-//    unsigned char os[sizeof(m)];
-//    size_t size;
-//    element_to_oct(os, &size, z);
-//    print_unsigned_char(os, "test", size);
+/* --- 復号 --- */
+    
 
     /* --- 領域の解放 --- */
+    free(g_element_str);
+    mpz_clears(limit, a, r, NULL);
+    for(i=0;i<msg_len/sizeof(long);i++) mpz_clear(u[i]);
     point_clear(P);
     point_clear(Q);
+    point_clear(aQ);
+    point_clear(raQ);
     element_clear(g);
     pairing_clear(p);
+    
+    print_green_color("--- 正常終了 ---\n");
+}
+
+
+/* -----------------------------------------------
+ * mpz_tでランダムな値を生成する関数
+ * $0 生成した値を入れる変数
+ * $1 上限値
+ * 参考サイト https://sehermitage.web.fc2.com/etc/gmp_src.html
+ -----------------------------------------------*/
+void create_mpz_t_random(mpz_t op, const mpz_t n) {
+    gmp_randstate_t state;
+    gmp_randinit_default(state);
+    
+    struct timeval tv, tv2;
+    gettimeofday(&tv2, NULL);
+    
+    do {
+        gettimeofday(&tv, NULL);
+    } while (tv.tv_usec == tv2.tv_usec);
+    
+    gmp_randseed_ui(state, tv.tv_usec);
+    mpz_urandomm(op, state, n);
+    
+    gmp_randclear(state);
+}
+
+/* -----------------------------------------------
+ * unsigned char(SHA256でハッシュ化した値)を出力する関数
+ * $0 出力するu_char
+ * $1 データ名（出力の最初にprintされる）
+ * $2 データサイズ
+ -----------------------------------------------*/
+void print_unsigned_char(const unsigned char *uc, const char *dataName, const size_t size){
+    printf("\x1b[32m%s = \x1b[39m", dataName);
+    for (size_t i=0; i<size; i++){
+        printf("%02x", uc[i] );
+    }
+    printf("\n");
 }
 
 /* -----------------------------------------------
